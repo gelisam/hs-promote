f375fa1349a2f4b1fd9435e33ddc2b530e7d14c9
Samuel Gélineau <gelisam@gmail.com>
2013-03-31 16:40:50 -0400

    Float
    Introducing Float between Int and Double, to have a slightly more
    interesting promotion hierarchy.

--- src/test.hs
+++ src/test.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeFamilies, FlexibleContexts,
+{-# LANGUAGE MultiParamTypeClasses, TypeFamilies,
              TemplateHaskell #-}
 
 import Promote
@@ -8,16 +8,31 @@ import GenInstances
 i42 :: Int
 i42 = 42
 
+f42 :: Float
+f42 = 42
+
 d42 :: Double
 d42 = 42
 
 
-$(genInstances [''Int, ''Double])
+$(genInstances [''Int, ''Float, ''Double])
 $(promote_both ''Int ''Double ['fromIntegral])
+$(promote_both ''Int ''Float ['fromIntegral])
+$(promote_both ''Float ''Double ['realToFrac])
 
 main = do print $ i42 + i42  -- typechecks
+          print $ f42 + f42  -- typechecks
           print $ d42 + d42  -- typechecks
           -- print $ i42 + d42  -- does not typecheck
-          print $ i42 `add` i42  -- typechecks
-          print $ d42 `add` d42  -- typechecks
-          print $ i42 `add` d42  -- typechecks!
+          
+          print $ i42 `add` i42
+          print $ i42 `add` f42
+          print $ i42 `add` d42
+          
+          print $ f42 `add` i42
+          print $ f42 `add` f42
+          print $ f42 `add` d42
+          
+          print $ d42 `add` i42
+          print $ d42 `add` f42
+          print $ d42 `add` d42
