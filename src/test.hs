de9aee9f9453a18b5918381dfa933b587c86b7dd
Samuel Gélineau <gelisam+github@gmail.com>
2013-03-31 21:55:59 -0400

    Fixed-precision Ints
    Now supporting Int8, Int16, Int32 and Int64.
    The spec says that an Int has an unspecified precision of at least
    2^29-1, but on my machine they are Int64, so I placed Int above Int64 in
    my numeric tower. If your machine behaves differently, all you have to
    do is to reorder the list, and all the correct promotions will be
    re-generated.

--- src/test.hs
+++ src/test.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, TypeFamilies,
              TemplateHaskell #-}
 
+import Data.Int
 import Promote
 import GenInstances
 
@@ -15,8 +16,13 @@ d42 :: Double
 d42 = 42
 
 
-$(promote_all [''Int, ''Float, ''Double]
-              ['fromIntegral, 'realToFrac])
+$(promote_all [''Int8, ''Int16, ''Int32, ''Int64, ''Int, ''Float, ''Double]
+              ['fromIntegral, -- Int8 to...
+               'fromIntegral, -- Int16 to...
+               'fromIntegral, -- Int32 to...
+               'fromIntegral, -- Int64 to...
+               'fromIntegral, -- Int to Float
+               'realToFrac])
 
 main = do print $ i42 + i42  -- typechecks
           print $ f42 + f42  -- typechecks
@@ -34,3 +40,5 @@ main = do print $ i42 + i42  -- typechecks
           print $ d42 `add` i42
           print $ d42 `add` f42
           print $ d42 `add` d42
+          
+          print $ (3 :: Int8) `add` (40000 :: Int32)
